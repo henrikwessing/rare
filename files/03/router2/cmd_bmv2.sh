5664c9cbd0e837665af5fcf7230ce8e410bc1d01
Henrik Wessing <hewe@fotonik.dtu.dk>
2025-08-14 16:52:02 +0200

    Finally lab 03 working componentwise

---
+++ files/03/router2/cmd_bmv2.sh
@@ -0,0 +1,1 @@
+simple_switch_grpc -i 2@h2 -i 12@r12 -i 64@veth250 --device-id 0 --log-console build/router.json -- --grpc-server-addr 127.0.0.1:50051
---
+++ images/router/cmd_ctrlchannel.sh
@@ -0,0 +1,3 @@
+ip link add veth251 type veth peer name veth250
+ip link set veth250 up 
+ip link set veth251 up
